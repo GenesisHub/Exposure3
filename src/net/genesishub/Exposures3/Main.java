2845f1f2cf85ef618dbe54cb31aa0e2daac598c5
Seshpenguin <seshan10@me.com>
2015-03-29 11:02:26 -0400

    Changes

--- src/net/genesishub/Exposures3/Main.java
+++ src/net/genesishub/Exposures3/Main.java
@@ -8,8 +8,8 @@ public class Main {
 	@SuppressWarnings("all")
 	public static void main(String args[]) throws Exception{
 		
-		String VERSION = "0.3-DEV"; //Exposure Server Version
-		String APIVERSION = "0.2-DEV"; //Exposure API Version
+		String VERSION = "0.4-DEV"; //Exposure Server Version
+		String APIVERSION = "0.3-DEV"; //Exposure API Version
 		
 		System.out.println("[INFO] Starting Exposure3 " + VERSION + " (Implementing Exposure API version " + APIVERSION + ")");
 		System.out.println("[INFO] Enabling Plugins...");
