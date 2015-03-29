21efc7f45e7aab250bce717d4e06d235dcfef919
Seshpenguin <seshan10@me.com>
2015-03-29 10:43:17 -0400

    Version Variable Changes

--- src/net/genesishub/Exposures3/Main.java
+++ src/net/genesishub/Exposures3/Main.java
@@ -5,8 +5,8 @@ public class Main {
 	@SuppressWarnings("all")
 	public static void main(String args[]) throws Exception{
 		
-		String VERSION = "0.1-DEV"; //Exposure Server Version
-		String APIVERSION = "0.1-DEV"; //Exposure API Version
+		String VERSION = "0.3-DEV"; //Exposure Server Version
+		String APIVERSION = "0.2-DEV"; //Exposure API Version
 		
 		System.out.println("[INFO] Starting Exposure3 " + VERSION + " (Implementing Exposure API version " + APIVERSION + ")");
 		System.out.println("[INFO] Enabling Plugins...");
