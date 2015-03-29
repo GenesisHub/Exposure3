ce39cd087358e43595456a78160545451d7b11ec
aguy867 <aguy867@gmail.com>
2015-03-28 22:58:41 -0400

    Changes

--- src/net/genesishub/Exposures3/ExposurePlugin.java
+++ src/net/genesishub/Exposures3/ExposurePlugin.java
@@ -1,5 +1,21 @@
 package net.genesishub.Exposures3;
 
-public class ExposurePlugin {
+import net.genesishub.Exposures3.PluginManager.State;
+
+public class ExposurePlugin implements Plugin{
+	
+	public static String name = "ExposurePlugin";
+	public static String version = "0.0.1";
+	public static State state;
+	
+	@Override
+	public void Enable() {
+		System.out.println("Plugin Loaded!");
+	}
+
+	@Override
+	public void Disable() {
+		System.out.println("Plugin Disabled!");
+	}
 
 }
--- src/net/genesishub/Exposures3/Plugin.java
+++ src/net/genesishub/Exposures3/Plugin.java
@@ -3,9 +3,9 @@ package net.genesishub.Exposures3;
 import net.genesishub.Exposures3.PluginManager.State;
 
 public interface Plugin {
-	public static State state = null;
-	public static String name = null;
-	public static String version = null;
+	public static State state = State.DISABLE;
+	public static String name = "Default";
+	public static String version = "0.0.0";
 	public void Enable();
 	public void Disable();
 }
